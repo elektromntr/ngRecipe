c1c80d3db92fad8514b93dc5e7d2acbe652377cf
Bartosz Wójcik <bwoj02@si-consulting.pl>
2021-03-16 10:21:26 +0100

    Recipes and ingeredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Lasagne', 'Try this at home', 'https://kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/lasagne_bolognese_2.jpg'),
+    new Recipe('Spaghetti', 'Try this at home', 'http://www.warzywizm.pl/wp-content/uploads/2020/01/A-WYBRANE-P1128074-auto-30-scaled.jpg')
+  ];
 
   constructor() { }
 
